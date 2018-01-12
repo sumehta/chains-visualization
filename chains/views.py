253ec29437ee1a8cfac01aca276eb66b8f2c1a04
Sneha Mehta <snehamehta@vt.edu>
2018-01-11 20:23:20 -0500

    update Procfile

--- chains/views.py
+++ chains/views.py
@@ -11,24 +11,24 @@ from chains.models import Story, Chain
 
 
 def index(request):
-    if request.method == 'GET':
-        publishers = dict()
-        for ch in Chain.objects.all():
-            if ch.story_id.publisher not in publishers:
-                publishers[ch.story_id.publisher] = []
-            publishers[ch.story_id.publisher].append(ch.story_id.id)
-        return render(request, 'chains/index.html', {'publishers': publishers})
+    # if request.method == 'GET':
+    publishers = dict()
+    for ch in Chain.objects.all():
+        if ch.story_id.publisher not in publishers:
+            publishers[ch.story_id.publisher] = []
+        publishers[ch.story_id.publisher].append(ch.story_id.id)
+    return render(request, 'chains/index.html', {'publishers': publishers})
 
 
 def view_chain(request, story_id):
-    if request.method == 'GET':
-        chains = eval(Chain.objects.get(story_id=story_id).chains)
-        flattened_chains = []
-        story = Story.objects.get(id=story_id)
-        raw_text = story.text
-        publisher = story.publisher
-        country = story.country
-        for chain in chains:
-            flattened_chains.append([(q_a[1], q_a[2]) for q_a in chain])
-        return render(request, 'chains/chain.html', {'chains': flattened_chains, 'text': raw_text,
-                                                     'publisher': publisher, 'country': country})
+    # if request.method == 'GET':
+    chains = eval(Chain.objects.get(story_id=story_id).chains)
+    flattened_chains = []
+    story = Story.objects.get(id=story_id)
+    raw_text = story.text
+    publisher = story.publisher
+    country = story.country
+    for chain in chains:
+        flattened_chains.append([(q_a[1], q_a[2]) for q_a in chain])
+    return render(request, 'chains/chain.html', {'chains': flattened_chains, 'text': raw_text,
+                                                 'publisher': publisher, 'country': country})
--- chains_visualization/settings.py
+++ chains_visualization/settings.py
@@ -25,19 +25,19 @@ SECRET_KEY = '5cjkw&t^7h)c_1*$f%fm!a9#w00a86z$d2&39#k6wq%3j0-hwt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'chains.apps.ChainsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chains.apps.ChainsConfig',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
+
