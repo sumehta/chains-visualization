e4a91d420711e5be4119acd52e33414fc37d98e2
Sneha Mehta <snehamehta@vt.edu>
2018-01-15 17:48:27 -0500

    update urls and views

--- chains/urls.py
+++ chains/urls.py
@@ -5,6 +5,7 @@ app_name = 'chains'
 urlpatterns = [
     path('', views.index, name='index'),
     path('view_chain/<int:story_id>/', views.view_chain, name='view_chain'),
+    path('get_details/<int:story_id>/', views.get_details, name='get_details')
     # path('<int:question_id>/', views.detail, name='detail'),
     # path('<int:question_id>/results/', views.results, name='results'),
     # path('<int:question_id>/vote/', views.vote, name='vote'),
--- chains/views.py
+++ chains/views.py
@@ -4,31 +4,50 @@ import os
 import json
 from collections import defaultdict
 
-from chains.models import Story, Chain
-
-
-# Create your views here.
+from chains.models import Story, Chain, Clause, Question, Sentence
 
 
 def index(request):
-    # if request.method == 'GET':
-    publishers = dict()
-    for ch in Chain.objects.all():
-        if ch.story_id.publisher not in publishers:
-            publishers[ch.story_id.publisher] = []
-        publishers[ch.story_id.publisher].append(ch.story_id.id)
-    return render(request, 'chains/index.html', {'publishers': publishers})
+    if request.method == 'GET':
+        publishers = dict()
+        for ch in Chain.objects.all():
+            if ch.story.publisher not in publishers:
+                publishers[ch.story.publisher] = []
+            publishers[ch.story.publisher].append(ch.story.id)
+        return render(request, 'chains/index.html', {'publishers': publishers})
 
 
 def view_chain(request, story_id):
-    # if request.method == 'GET':
-    chains = eval(Chain.objects.get(story_id=story_id).chains)
-    flattened_chains = []
-    story = Story.objects.get(id=story_id)
-    raw_text = story.text
-    publisher = story.publisher
-    country = story.country
-    for chain in chains:
-        flattened_chains.append([(q_a[1], q_a[2]) for q_a in chain])
-    return render(request, 'chains/chain.html', {'chains': flattened_chains, 'text': raw_text,
-                                                 'publisher': publisher, 'country': country})
+    if request.method == 'GET':
+        chains = eval(Chain.objects.get(story_id=story_id).chains)
+
+        flattened_chains = []
+        story = Story.objects.get(id=story_id)
+        raw_text = story.text
+        publisher = story.publisher
+        country = story.country
+        for chain in chains:
+            flattened_chains.append([(q_a[1], q_a[2]) for q_a in chain])
+        return render(request, 'chains/chain.html', {'chains': flattened_chains, 'text': raw_text,
+                                                     'publisher': publisher, 'country': country, 'story_id': story_id})
+
+
+def get_details(request, story_id):
+    if request.method == 'GET':
+        sentences = eval(Sentence.objects.get(story=story_id).sentences)
+        questions = eval(Question.objects.get(story=story_id).questions)
+
+        clauses = defaultdict(list)
+        cl = Clause.objects.get(story=story_id)
+        clauses_raw = eval(cl.clauses)
+        for line in clauses_raw:
+            tokens = line.strip('\n').split('\t')
+            sent_no, clause = int(tokens[0]), ' '.join([tok[1:-1] for tok in tokens])
+            clauses[sent_no].append(clause)
+
+        sent_clause_questions = defaultdict(list)
+
+        for k, v in questions.items():
+            sent_clause_questions[int(k)].append((sentences[int(k)], clauses[int(k)], questions[k]))
+
+        return render(request, 'chains/chain_details.html', {'story_id': story_id, 'sent_clause_questions': dict(sent_clause_questions)})
